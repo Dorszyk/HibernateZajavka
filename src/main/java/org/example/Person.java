72a79d6f2b226a22be8921244796ae6425ef0470
Piotr Dorszynski <piotr.dorszynski@gmail.com>
2023-03-11 13:55:46 +0100

    hibernate.cfg

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "person")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "person_id")
+    private Integer personId;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "age")
+    private Integer age;
+
+}
+
